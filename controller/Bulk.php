b4e4bf489d5f6a98191792f8e795698a1e92a23b
thomas <thomas@barney.im>
2017-03-24 18:42:41 +0100

    m

--- controller/Bulk.php
+++ controller/Bulk.php
@@ -45,7 +45,7 @@ class Bulk extends Controller
       foreach($dom as $k=>$v)
       {
 
-        $minutes = (6+(($v['note']*$v['note']*$v['note'])/2));
+        $minutes = (60+(($v['note']*$v['note']*$v['note'])/2));
         $diff =   strtotime("-".$minutes." minutes", $_SERVER['REQUEST_TIME'])-$v['lastsend'];
         if($diff > 0 && $i < $e[0])
         {
