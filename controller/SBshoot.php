b8885f384637455f5e9e37bf21b224bb60e64713
thomas <thomas@barney.im>
2017-04-12 14:54:05 +0200

    m

--- controller/SBshoot.php
+++ controller/SBshoot.php
@@ -24,8 +24,8 @@ class SBshoot extends Controller
 
     public function test()
     {
-    echo  shell_exec('curl --url "smtps://smtp.mail.yahoo.com:465" --mail-from "huiporrelo1982@yahoo.com" --mail-rcpt "mrsoyer@me.com" --user "huiporrelo1982@yahoo.com:Eb6BJ7YJqB" --insecure --upload-file mail.txt --verbose
-');
+    print_r(shell_exec('curl --url "smtps://smtp.mail.yahoo.com:465" --mail-from "huiporrelo1982@yahoo.com" --mail-rcpt "mrsoyer@me.com" --user "huiporrelo1982@yahoo.com:Eb6BJ7YJqB" --insecure --upload-file mail.txt --verbose
+'));
     }
 
     public function parser()
