ef14413045a6c3695057468002a8bfc13cf27a53
thomas <thomas@barney.im>
2017-04-12 18:41:51 +0200

    m

--- controller/Rotator.php
+++ controller/Rotator.php
@@ -38,6 +38,7 @@ class Rotator extends Controller
         //print_r($people);
         if(!isset($people['_id']['$oid'])) die();
         $shoot = $this->sendPeople($people,$sym);
+        sleep(5);
         print_r($shoot);
         if($shoot != "ok")
         {
