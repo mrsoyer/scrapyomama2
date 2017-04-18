52cb65b28fa50ce16e90a77b7e735f1366e3aca2
thomas <thomas@barney.im>
2017-04-18 11:43:23 +0200

    m

--- controller/Rotator.php
+++ controller/Rotator.php
@@ -42,7 +42,7 @@ class Rotator extends Controller
       $sym['proxy'] = $this->proxy();
 
       //$sym['link'] = $_SERVER['SERVER_NAME'];
-      $sym['link'] = "scrapyomama.herokuapp.com/Trck/c/".md5(uniqid(time()));
+      $sym['link'] = $_SERVER['SERVER_NAME']."/Trck/c/".md5(uniqid(time()));
       $sym['link'] = $this->tinyurl($sym['link']);
       $sym['e']=0;
       $this->shoot($sym);
