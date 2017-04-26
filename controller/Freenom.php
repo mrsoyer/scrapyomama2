bee04febef791a0cc871519579a2d57548cbf095
thomas <thomas@barney.im>
2017-04-26 10:28:17 +0200

    m

--- controller/Freenom.php
+++ controller/Freenom.php
@@ -23,6 +23,7 @@ class Freenom extends Controller
     public function createdom($e)
     {
         $dom = $this->tinyurl($e[0]);
+        $dom = $e[0];
         $json = json_decode(shell_exec('curl -X POST https://api.freenom.com/v2/domain/register -d "forward_url='.$dom.'"'),true);
         $this->loadModel('Freenoms');
         $this->Freenoms->addDom($json['domain'][0]['domainname']);
